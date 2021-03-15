be1d3eb105e9c003c6db9632dc67d316e6468e2a
Irina Rusinova <rusinovarusya@mail.ru>
2021-03-15 10:48:54 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  if (typeof array == Number) return array;
+  else {
+    let min = array[0];
+    for (let key of array) {
+      if (key < min) min = key;
+    }
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  if (typeof array == Number) return array;
+  else {
+    let max = array[0];
+    for (let key of array) {
+      if (key > max) max = key;
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  if (typeof array == Number) return array;
+  else {
+    let sumItems = array.reduce((sum, current) => sum +current, 0);
+    return sumItems / array.length;
+  }
 }
